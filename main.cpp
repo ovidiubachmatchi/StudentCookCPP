27abed786c80af4e50fbafc86449bb811069830a
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2021-05-19 14:02:07 +0300

    QT Update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include "functions.h"
-#include "classes.h"
-using namespace std;
-int main() {
-    test();
-    Recipe test1;
-    Recipe test2(5);
-    cout<<test1.get_x()<<endl;
-    cout<<test2.get_x()<<endl;
-    system("pause");
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
